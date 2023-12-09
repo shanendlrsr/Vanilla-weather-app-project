db776388f6157b9e4873f8cdf582d89098e59a69
Shanen Del Rosario <shanendelrosario0@gmail.com>
2023-12-09 19:05:30 +1000

    Added more weather data
    Updated:
    - current date using API
    - weather description
    - humidity
    - wind

--- src/app.js
+++ src/app.js
@@ -1,10 +1,49 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let format = `${day} ${hours}:${minutes}`;
+  return format;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature-value");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let descriptionElement = document.querySelector("#description");
+  let description = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  dateElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind} km/h`;
 }
 
 function searchCity(city) {
